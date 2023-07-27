97101f246a7dd986ce4ae452b6f4539eab3b7ec5
HAMZAMSABRI2 <98179432+HAMZAMSABRI2@users.noreply.github.com>
2023-07-27 15:25:14 +0100

    styling

--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
 getSurat();
-let boxsalat = document.querySelector('.box')
+let boxsalat = document.querySelector(".box");
 
 function getSurat() {
-
-    fetch("http://api.alquran.cloud/v1/meta")
-        .then(response => response.json())
-        .then(data => {
-
-            let surat = data.data.surahs.references;
-            let number = 114;
-            for (let i = 0; i < number; i++) {
-                console.log(surat[i]);
-                boxsalat.innerHTML +=
-                    `
+  fetch("http://api.alquran.cloud/v1/meta")
+    .then((response) => response.json())
+    .then((data) => {
+      let surat = data.data.surahs.references;
+      let number = 114;
+      for (let i = 0; i < number; i++) {
+        console.log(surat[i]);
+        boxsalat.innerHTML += `
                 <div class="salat">
+                <p>${surat[i].numberOfAyahs}</p>
                 <p>${surat[i].name}</p>
                 <div class="number">
                 <p class="nombre">${surat[i].number}</p>
                  </div>   
                     </div>
-                `
-
-            }
-        })
-
+                `;
+      }
+    });
 }
