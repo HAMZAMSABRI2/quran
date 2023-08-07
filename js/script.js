7b32024bbee0da14b450dbe065a528133ed02c3e
HAMZAMSABRI2 <98179432+HAMZAMSABRI2@users.noreply.github.com>
2023-08-07 14:20:07 +0100

    quran

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-getSurat();
+function searchSurat() {
+  let input = document.getElementById("surah-input");
+  let filter = input.value;
+  let suratItems = document.querySelectorAll(".salat");
+
+  suratItems.forEach((item) => {
+    let suratName = item
+      .querySelector("p:nth-child(2)")
+      .textContent.toLowerCase();
+    if (suratName.includes(filter).toLowerCase()) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
 let boxsalat = document.querySelector(".box");
 
 function getSurat() {
@@ -8,16 +23,42 @@ function getSurat() {
       let surat = data.data.surahs.references;
       let number = 114;
       for (let i = 0; i < number; i++) {
-        console.log(surat[i]);
         boxsalat.innerHTML += `
-                <div class="salat">
-                <p>${surat[i].numberOfAyahs}</p>
-                <p>${surat[i].name}</p>
-                <div class="number">
-                <p class="nombre">${surat[i].number}</p>
-                 </div>   
-                    </div>
-                `;
+          <div class="salat">
+            <p>${surat[i].numberOfAyahs}</p>
+            <p class="sora">${surat[i].name}</p>
+            <div class="number">
+              <p class="nombre">${surat[i].number}</p>
+            </div>   
+          </div>
+        `;
       }
+
+
+      let SurahsTitels = document.querySelectorAll(".salat");
+      let popup = document.querySelector(".surah-popup"),
+        AyatContainer = document.querySelector(".ayat");
+      SurahsTitels.forEach((title, index) => {
+        title.addEventListener("click", () => {
+          fetch(`http://api.alquran.cloud/v1/surah/${index + 1}`)
+            .then((response) => response.json())
+            .then((data) => {
+              AyatContainer.innerHTML = "";
+              let Ayat = data.data.ayahs;
+              Ayat.forEach((aya) => {
+                popup.classList.add("active");
+                AyatContainer.innerHTML += `
+                          <p>(${aya.numberInSurah}) - ${aya.text}</p>
+                      `;
+              });
+            });
+        });
+      });
+      let closePopup = document.querySelector(".close-popup");
+      closePopup.addEventListener("click", () => {
+        popup.classList.remove("active");
+      });
     });
 }
+
+getSurat();
