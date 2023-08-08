14797191111f22458ad622ef64b37377c2e818d9
HAMZAMSABRI2 <98179432+HAMZAMSABRI2@users.noreply.github.com>
2023-08-08 18:14:58 +0100

    audio

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function searchSurat() {
     let suratName = item
       .querySelector("p:nth-child(2)")
       .textContent.toLowerCase();
-    if (suratName.includes(filter).toLowerCase()) {
+    if (suratName.toLowerCase().includes(filter)) {
       item.style.display = "flex";
     } else {
       item.style.display = "none";
@@ -34,12 +34,32 @@ function getSurat() {
         `;
       }
 
-
       let SurahsTitels = document.querySelectorAll(".salat");
+      let sora = document.querySelector(".sora");
+      let mp3 = document.querySelector(".audio");
+
       let popup = document.querySelector(".surah-popup"),
         AyatContainer = document.querySelector(".ayat");
       SurahsTitels.forEach((title, index) => {
+        console.log(index);
         title.addEventListener("click", () => {
+          fetch("http://api.alquran.cloud/v1/quran/ar.alafasy")
+            .then((response) => response.json())
+            .then((data) => {
+              let qari = data.data.surahs;
+              let qariAudio = qari[index].ayahs[0].audio;
+              if ((title[index] = qari[index])) {
+                mp3.innerHTML = `
+                <audio controls>
+                <source src="${qariAudio}" type="audio/mp3">
+               
+              </audio>
+              `;
+              } else {
+                console.log("hh ");
+              }
+            });
+          // console.log(sora.textContent);
           fetch(`http://api.alquran.cloud/v1/surah/${index + 1}`)
             .then((response) => response.json())
             .then((data) => {
@@ -47,7 +67,10 @@ function getSurat() {
               let Ayat = data.data.ayahs;
               Ayat.forEach((aya) => {
                 popup.classList.add("active");
+
                 AyatContainer.innerHTML += `
+                
+                
                           <p>(${aya.numberInSurah}) - ${aya.text}</p>
                       `;
               });
@@ -59,6 +82,81 @@ function getSurat() {
         popup.classList.remove("active");
       });
     });
+
+  // let SurahsTitels = document.querySelectorAll(".salat");
+
+  // console.log(SurahsTitels[0])
+  // Retrieve id from clicked element
 }
 
 getSurat();
+
+// function dropdown() {
+//   let select = document.querySelector(".form-select");
+
+//   let mp3 = document.querySelector(".audio");
+
+//   let popup = document.querySelector(".surah-popup");
+
+//   let SurahsTitels = document.querySelectorAll(".salat");
+
+//   console.log(SurahsTitels[0].querySelector(".sora").textContent)
+//     // Retrieve id from clicked element
+
+//   fetch(
+//     "http://api.alquran.cloud/v1/quran/ar.alafasy"
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       let qari = data.data.surahs;
+
+//     });
+
+// }
+
+// dropdown();
+
+/*
+fetch(`http://api.alquran.cloud/v1/meta`)
+.then((response) => response.json())
+.then((data) => {
+  let surat = data.data.surahs.references;
+  surat.forEach((aya , i) => {
+    if (title.name || aya[i].name) { 
+      mp3.innerHTML += `
+    <source src="${title.format}" type="audio/ogg">
+    `;
+    }
+  });
+
+
+
+  ======================================================
+
+
+    // Wrap your JavaScript code in an event listener for DOMContentLoaded
+  document.addEventListener("DOMContentLoaded", function () {
+    // const select = document.getElementById("select");
+    // const mp3 = document.getElementById("mp3");
+
+    select.addEventListener("change", function () {
+      let selectedIdentifier = select.value;
+
+      
+        .then((response) => response.json())
+        .then((data) => {
+          let surat = data.data.surahs.references;
+
+          surat.forEach((aya) => {
+            console.log(aya.audio)
+            if (aya.number == selectedIdentifier) {
+              // Use 'number' instead of 'identifier'
+              
+              mp3.innerHTML = `<source src="${aya.audio}" type="audio/ogg">`;
+              mp3.load(); // Reload the audio element to load the new source
+            }
+          });
+        });
+    });
+  });
+*/
