eb07c484d1825412538be39ab233bce1cd73a691
Hamza Msabri <98179432+HAMZAMSABRI2@users.noreply.github.com>
2023-08-16 14:51:40 +0100

    edit media player

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
+let boxsalat = document.querySelector(".box");
+
+let qarii = document.querySelector('#muqri').value;
+
+const surahs = document.getElementById('surahs');
+
+const result = document.getElementById('result');
+
+function getSurat() {
+  fetch("http://api.alquran.cloud/v1/meta")
+    .then((response) => response.json())
+    .then((data) => {
+      let surat = data.data.surahs.references;
+      let number = 114;
+      for (let i = 0; i < number; i++) {
+        boxsalat.innerHTML += `
+            <div class="salat"  onclick="showSurat('${i + 1}')">
+              <p>${surat[i].numberOfAyahs}</p>
+              
+              
+              <p class="sora">${surat[i].name}</p>
+              <div class="number">
+              <p class="nombre">${surat[i].number}</p>
+              </div>   
+
+              </div>
+          `;
+      }
+      surat.add;
+    });
+}
+let j 
+
+var selectElement = document.getElementById("muqri");
+var selectedValue = selectElement.value;
+function handleSelectChange() {
+  selectedValue = selectElement.value;
+  showSurat(j)
+}
+
+
 function searchSurat() {
   let input = document.getElementById("surah-input");
   let filter = input.value;
@@ -8,195 +49,91 @@ function searchSurat() {
       .querySelector("p:nth-child(2)")
       .textContent.toLowerCase();
     if (suratName.toLowerCase().includes(filter)) {
-      // item.style.display = "grid";
+      item.style.display = "grid";
     } else {
       item.style.display = "none";
     }
   });
 }
-let boxsalat = document.querySelector(".box");
+getSurat();
 
-function getSurat() {
-  fetch("http://api.alquran.cloud/v1/meta")
+
+function showSurat(id ) {
+  j = id
+
+
+  
+
+  let mp3 = document.querySelector(".audio");
+
+  let AyatContainer = document.querySelector(".ayat");
+  let muqria
+  fetch(`http://api.alquran.cloud/v1/surah/${id}`)
     .then((response) => response.json())
     .then((data) => {
-      let surat = data.data.surahs.references;
-      let number = 114;
-      for (let i = 0; i < number; i++) {
-        boxsalat.innerHTML += `
+      AyatContainer.innerHTML = "";
+      let Ayat = data.data.ayahs;
+      let number = data.data;
+      Ayat.forEach((aya) => {
+        AyatContainer.innerHTML += `
 
-          <div class="salat">
-            <p>${surat[i].numberOfAyahs}</p>
-            <p class="sora">${surat[i].name}</p>
+         
+             
+                       <p class="ayat">(${aya.numberInSurah}) - ${aya.text}</p>
+                       
+                   `;
+      });
+      $("#myModal").modal("show");
+
+      fetch("../data/quran.json")
+        .then((response) => response.json())
+        .then((data) => {
+          let voice = data[2].data
+
+
+
+          console.log(selectedValue);
+          muqria = voice[selectedValue]
+
+          console.log(muqria);
+          
+
+          if (id !== number.number) {
+
             
-            <div class="number">
-            <p class="nombre">${surat[i].number}</p>
-            </div>   
-            </div>
-        `;
-      }
+            mp3.innerHTML =
 
-      let SurahsTitels = document.querySelectorAll(".salat");
-      let sora = document.querySelector(".sora");
-      let mp3 = document.querySelector(".audio");
+              `
 
-      let popup = document.querySelector(".surah-popup"),
-        AyatContainer = document.querySelector(".ayat");
-      SurahsTitels.forEach((title, index) => {
+              <audio controls  class="audio w-100">
+                <source src="${muqria[id].audio}" type="audio/mp3">
 
-        title.addEventListener("click", () => {
-          fetch("http://api.alquran.cloud/v1/quran/ar.alafasy")
-            .then((response) => response.json())
-            .then((data) => {
-              let qari = data.data.surahs;
-              let qariAudio = qari[index].ayahs[0].audio;
-              if ((title[index] = qari[index])) {
-                mp3.innerHTML = `
-                <audio controls>
-                <source src="${qariAudio}" type="audio/mp3">
-               
+
               </audio>
-              `;
-              } else {
-                console.log("hh ");
-              }
-            });
-          // console.log(sora.textContent);
-          // SurahsTitels
-          fetch(`http://api.alquran.cloud/v1/surah/${index + 1}`)
-            .then((response) => response.json())
-            .then((data) => {
-              console.log(title);
-              AyatContainer.innerHTML = "";
-              let Ayat = data.data.ayahs;
+            
+            `
 
-              // let name = data.data.name;
-              Ayat.forEach((aya) => {
-                popup.classList.add("active");
+            surahs.innerHTML = 
+            
+            `
+                <h1>${muqria[id].english_name}</h1>
+            `
+            // muqria.forEach((surah)=>{
+              
+            // })
 
-                AyatContainer.innerHTML += `
-                
-                          <h1></h1>
-                          <p>(${aya.numberInSurah}) - ${aya.text}</p>
-                          
-                      `;
-              });
-            });
-        });
-      });
-      let closePopup = document.querySelector(".close-popup");
-      closePopup.addEventListener("click", () => {
-        popup.classList.remove("active");
-      });
+          } else {
+            console.log("error");
+          }
+        })
     });
+    
 
-  // let SurahsTitels = document.querySelectorAll(".salat");
 
-  // console.log(SurahsTitels[0])
-  // Retrieve id from clicked element
+
+  
+
+
 }
 
 
-
-getSurat();
-
-// function dropdown() {
-//   let select = document.querySelector(".form-select");
-
-//   let mp3 = document.querySelector(".audio");
-
-//   let popup = document.querySelector(".surah-popup");
-
-//   let SurahsTitels = document.querySelectorAll(".salat");
-
-//   console.log(SurahsTitels[0].querySelector(".sora").textContent)
-//     // Retrieve id from clicked element
-
-//   fetch(
-//     "http://api.alquran.cloud/v1/quran/ar.alafasy"
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       let qari = data.data.surahs;
-
-//     });
-
-// }
-
-// dropdown();
-
-/*
-fetch(`http://api.alquran.cloud/v1/meta`)
-.then((response) => response.json())
-.then((data) => {
-  let surat = data.data.surahs.references;
-  surat.forEach((aya , i) => {
-    if (title.name || aya[i].name) { 
-      mp3.innerHTML += `
-    <source src="${title.format}" type="audio/ogg">
-    `;
-    }
-  });
-let qari = data.data.surahs;
-
-      let audioHTML = ''; // Variable to store the HTML for audio elements
-
-      qari.forEach((surah, index) => {
-        let qariAudio = surah.ayahs.map((aya) => aya.audio); // Create an array of audio URLs for each ayah in the surah
-        
-        qariAudio.forEach((audioUrl) => {
-          audioHTML += `
-            <audio controls>
-              <source src="${audioUrl}" type="audio/mp3">
-              Your browser does not support the audio element.
-            </audio>
-            <br>
-          `;
-        });
-      });
-
-      mp3.innerHTML = audioHTML; // Set the innerHTML of mp3 element to the generated audio elements
-
-In this corrected code, we build up the audioHTML string inside the loop, and after looping through all surahs, we set the innerHTML of the mp3 element to the accumulated audioHTML string.
-
-Please ensure you have elements with the IDs "title" and "mp3" in your HTML code to match the code above. Also, make sure that your browser allows fetching resources from external domains (CORS policy).
-
-
-
-
-
-
-
-
-
-
-
-  ======================================================
-
-
-    // Wrap your JavaScript code in an event listener for DOMContentLoaded
-  document.addEventListener("DOMContentLoaded", function () {
-    // const select = document.getElementById("select");
-    // const mp3 = document.getElementById("mp3");
-
-    select.addEventListener("change", function () {
-      let selectedIdentifier = select.value;
-
-      
-        .then((response) => response.json())
-        .then((data) => {
-          let surat = data.data.surahs.references;
-
-          surat.forEach((aya) => {
-            console.log(aya.audio)
-            if (aya.number == selectedIdentifier) {
-              // Use 'number' instead of 'identifier'
-              
-              mp3.innerHTML = `<source src="${aya.audio}" type="audio/ogg">`;
-              mp3.load(); // Reload the audio element to load the new source
-            }
-          });
-        });
-    });
-  });
-*/
